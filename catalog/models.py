7c8f60f6b306cf653120571c3711c42aca78f268
papaps <45527966+papaps@users.noreply.github.com>
2020-03-19 18:00:29 +0800

    Added User and Review class to models.py

--- catalog/models.py
+++ catalog/models.py
@@ -1,23 +1,26 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Publisher(models.Model):
     """Model representing a book genre"""
     name = models.CharField(max_length=200, help_text='Enter the name of the publisher')
-    
+
     def __str__(self):
         """String for representing the Model object."""
         return self.name
 
-from django.urls import reverse # Used to generate URLs by reversing the URL patterns
+
+from django.urls import reverse  # Used to generate URLs by reversing the URL patterns
+
 
 class Book(models.Model):
     """Model representing a book (but not a specific copy of book)."""
     title = models.CharField(max_length=200)
 
-    #Foreign Key used because book can only have one author, but authors can have multiple books
-    #Author as a string rather than the object because it hasn't been declared yet in the file
+    # Foreign Key used because book can only have one author, but authors can have multiple books
+    # Author as a string rather than the object because it hasn't been declared yet in the file
     author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
     publisher = models.ForeignKey('Publisher', on_delete=models.SET_NULL, null=True)
 
@@ -27,19 +30,22 @@ class Book(models.Model):
     def __str__(self):
         """String for representing the Model object."""
         return self.title
-    
+
     def get_absolute_url(self):
         """Returns the url to access a detail record for this book."""
         return reverse('book-detail', args=[str(self.id)])
 
-import uuid # Required for unique book instances
+
+import uuid  # Required for unique book instances
+
 
 class BookInstance(models.Model):
     """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this particular book across whole library')
-    book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True) 
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                          help_text='Unique ID for this particular book across whole library')
+    book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
     imprint = models.CharField(max_length=200)
-    
+
     STATUS = (
         ('a', 'Available'),
         ('r', 'Reserved'),
@@ -60,12 +66,11 @@ class BookInstance(models.Model):
         """String for representing the Model object."""
         return f'{self.id} ({self.book.title})'
 
+
 class Author(models.Model):
     """Model representing an author."""
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    date_of_birth = models.DateField(null=True, blank=True)
-    date_of_death = models.DateField('Died', null=True, blank=True)
 
     class Meta:
         ordering = ['last_name', 'first_name']
@@ -77,3 +82,24 @@ class Author(models.Model):
     def __str__(self):
         """String for representing the Model object."""
         return f'{self.last_name}, {self.first_name}'
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    ID_number = models.CharField(max_length=8)
+
+    def __str__(self):
+        return f'{self.ID_number}'
+
+
+class Review(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    text = models.CharField(max_length=2000)
+    user = models.ForeignKey('User', on_delete=models.SET_NULL, null=True)
+    book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f'{self.id}'
