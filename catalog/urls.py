534980d708b585d61d00b003c6057af8984c1d14
martintierro15 <martintierro15@gmail.com>
2020-04-13 11:38:06 +0800

    Changed name of url to index

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='books'),
+    path('', views.index, name='index'),
     path('signup/', views.SignupView.as_view(), name='signup'),
 ]
