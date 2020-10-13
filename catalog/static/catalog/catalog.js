658166e29726c356b992523799d69fc3018e6f46
papaps <45527966+papaps@users.noreply.github.com>
2020-10-13 11:43:07 +0800

    Completed book details missing reviews styling

--- catalog/static/catalog/catalog.js
+++ catalog/static/catalog/catalog.js
@@ -1,13 +1,19 @@
 $(document).ready(function() {
     $(".review_submit").attr('disabled', true);
+    $(".review_submit").css('color', '#969696');
+            $(".review_submit").css('background', '#dedede');
     
     $('textarea').on('keyup',function() {
         var textarea_value = $(".review_input").val();
         
         if(textarea_value != '') {
             $(".review_submit").attr('disabled', false);
+            $(".review_submit").css('color', 'white');
+            $(".review_submit").css('background', 'black');
         } else {
             $(".review_submit").attr('disabled', true);
+            $(".review_submit").css('color', '#969696');
+            $(".review_submit").css('background', '#dedede');
         }
     });
 });
