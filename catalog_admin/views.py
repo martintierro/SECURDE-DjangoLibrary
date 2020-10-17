5b55048e77ca47d679b8ad31b4b7cf17b6e079ce
Martin Tierro <martintierro15@gmail.com>
2020-10-17 11:54:28 +0800

    Added another thing

--- catalog_admin/views.py
+++ catalog_admin/views.py
@@ -12,7 +12,7 @@ def index(request):
     template = loader.get_template('catalog_admin/users.html')
     users = User.objects.filter(groups__name='Users')
     context = {
-        'users':users,
+        'users' : users,
     }
     return HttpResponse(template.render(context, request))
 
@@ -20,34 +20,6 @@ def view_managers(request):
     template = loader.get_template('catalog_admin/managers.html')
     managers = User.objects.filter(groups__name='Managers')
     context = {
-        'managers': managers,
+        'managers' : managers,
     }
     return HttpResponse(template.render(context, request))
-#     if request.method == 'POST':
-#         form = SearchForm(request.POST)
-#         if form.is_valid():
-#             query = form.cleaned_data['query']
-#             books = Book.objects.filter(Q(title__icontains=query) | Q(author__in=Author.objects.filter(Q(first_name__icontains=query) | Q(last_name__icontains=query))) )   
-#         else:
-#             return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
-#     else:
-#         books = Book.objects.all()
-#         form = SearchForm()
-#     template = loader.get_template('')
-#     instances = BookInstance.objects.all()
-#     status_dictionary = dict()
-#     for b in books:
-#         status = 'r'
-#         for i in instances:
-#             if i.book.isbn == b.isbn:
-#                 if i.status == 'a':
-#                     status = 'a'
-#         status_dictionary[b] = status
-
-#     context = {
-#         'books': books,
-#         'instances': instances,
-#         'status_dictionary': status_dictionary,
-#         'search_form': form,
-#     }
-#     return HttpResponse(template.render(context, request))
