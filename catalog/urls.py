5ae2635d4ca9d11e89a57ebeefe0bfc17f1e8f15
Martin Tierro <martintierro15@gmail.com>
2020-10-17 10:55:29 +0800

    Fixed redirecting of index

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='catalog_index'),
     path('signup/', views.SignupView.as_view(), name='signup'),
     path('<int:book_id>/', views.book_details, name="book_details"),
     path('<int:book_id>/reserve/', views.reserve_book, name="reserve_book"),
