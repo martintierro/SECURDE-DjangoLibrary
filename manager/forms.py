777dadcf1f4431b2b2c12d816fdb32bab96d5bbe
Martin Tierro <martintierro15@gmail.com>
2020-12-23 16:07:28 +0800

    Manager can now add books

--- manager/forms.py
+++ manager/forms.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from catalog.models import *
 from django import forms
 from django.utils.translation import ugettext_lazy as _
-
+from django.core.exceptions import ValidationError
 
 class ResetPasswordForm(forms.ModelForm):
 
@@ -26,3 +26,43 @@ class ResetPasswordForm(forms.ModelForm):
             self.add_error('confirm_password', "Password does not match")
 
         return cleaned_data
+
+class AddBookForm(forms.ModelForm):
+    title = forms.CharField(max_length=200, widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': 'Title'}))
+    # author = forms.ForeignKey(widget=forms.TextInput(
+    #     attrs={'class': 'form-control'}))
+    # publisher = forms.ForeignKey(widget=forms.TextInput(
+    #     attrs={'class': 'form-control'}))
+    year = forms.CharField(max_length=4, widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': 'Year of Publication'}))
+    isbn = forms.CharField(max_length=13, widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': '13 Character ISBN Number'}))
+    
+    class Meta:
+        model = Book
+        fields = ['title', 'year', 'isbn']
+
+class AddAuthorForm(forms.ModelForm):
+    first_name = forms.CharField(max_length=100, widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': 'First Name'}), required=False)
+    last_name = forms.CharField(max_length=100, widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': 'Last Name'}), required=False)
+
+    class Meta:
+        model = Author
+        fields = ['first_name', 'last_name']
+    
+    def clean(self):
+        cleaned_data = super(AddAuthorForm, self).clean()
+        if (cleaned_data.get('first_name') == '' and cleaned_data.get('last_name') != '') or (cleaned_data.get('first_name') != '' and cleaned_data.get('last_name') == ''):
+            raise ValidationError('Please fill out the empty author field')
+
+
+class AddPublisherForm(forms.ModelForm):
+    name = forms.CharField(max_length=200, widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': 'Publisher'}), required=False)
+
+    class Meta:
+        model = Publisher
+        fields = ['name']
--- manager/views.py
+++ manager/views.py
@@ -2,7 +2,7 @@ from django.template import loader
 from django.template.response import TemplateResponse
 from django.views.generic import View
 from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.hashers import check_password
 from catalog.models import *
@@ -39,10 +39,74 @@ def add_book(request):
     template = loader.get_template('manager/add_book.html')
     authors = Author.objects.all()
     publishers = Publisher.objects.all()
+
+    if request.method == 'POST':
+        book_form = AddBookForm(data=request.POST)
+        author_form = AddAuthorForm(data=request.POST)
+        publisher_form = AddPublisherForm(data=request.POST)
+        
+        selected_author = None   
+        selected_publisher = None   
+        
+        if book_form.is_valid():
+            if request.POST.get('author_id') is not None:
+                selected_author = get_object_or_404(Author, pk=request.POST.get('author_id'))
+            elif author_form.is_valid() and author_form.has_changed():
+                author = author_form.save(commit=False)
+                first_name = author_form.cleaned_data['first_name']
+                last_name = author_form.cleaned_data['last_name']
+                author.first_name = first_name
+                author.last_name = last_name
+                author.save()
+                selected_author = Author.objects.get(id=author.id)
+
+           
+            if request.POST.get('publisher_id') is not None:
+                selected_publisher = get_object_or_404(Publisher, pk=request.POST.get('publisher_id'))
+            elif publisher_form.is_valid() and publisher_form.has_changed():
+                publisher_object = publisher_form.save(commit=False)
+                publisher_name = publisher_form.cleaned_data['name']
+                publisher_object.name = publisher_name
+                publisher_object.save()
+                selected_publisher = Publisher.objects.get(id=publisher_object.id)
+            
+
+
+            if book_form.is_valid() and selected_publisher is not None and selected_author is not None:
+                book = book_form.save(commit=False)
+                
+
+                title = book_form.cleaned_data['title']
+                year = book_form.cleaned_data['year']
+                isbn = book_form.cleaned_data['isbn']
+                
+                book.title = title
+                book.author = selected_author
+                book.publisher = selected_publisher
+                book.year = year
+                book.isbn = isbn
+
+                book.save()
+
+                return redirect('manager_index')
+        else:
+            return redirect('add_book')
+
+
+
+    else:
+        book_form = AddBookForm()
+        author_form = AddAuthorForm()
+        publisher_form = AddPublisherForm()
+
     context = {
         'authors': authors,
         'publishers': publishers,
+        'book_form': book_form,
+        'publisher_form': publisher_form,
+        'author_form': author_form,
     }
+
     return HttpResponse(template.render(context, request))
 
 @user_passes_test(lambda u: u.groups.filter(name='Managers').exists(), login_url=reverse_lazy('login'))
