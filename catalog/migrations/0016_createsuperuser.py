c5f5142aefedda504b6d899837a6c96fd95228c9
Martin Tierro <martintierro15@gmail.com>
2020-12-28 15:52:51 +0800

    Create superuser on startup

---
+++ catalog/migrations/0016_createsuperuser.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+from django.contrib.auth.admin import User
+
+
+def create_superuser(apps, schema_editor):
+    superuser = User()
+    superuser.is_active = True
+    superuser.is_superuser = True
+    superuser.is_staff = True
+    superuser.username = 'admin'
+    superuser.email = 'admin@admin.net'
+    superuser.set_password('Delasalle1!')
+    superuser.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('catalog', '0015_auto_20201222_2140'),
+    ]
+
+    operations = [
+        migrations.RunPython(create_superuser)
+    ]
